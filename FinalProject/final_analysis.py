70458c1563633a4ccd41c084372d44b59e192bbd
skyhoffert <skwylarh@yahoo.com>
2018-11-11 13:50:35 -0500

    First part of amplifier design finished. More analysis to do.

--- FinalProject/final_analysis.py
+++ FinalProject/final_analysis.py
@@ -1,6 +1,7 @@
 # Sky Hoffert
 # November 9, 2018
 
+import cmath
 from math import *
 
 ##########################################################################################################################
@@ -58,26 +59,17 @@ f_off = 2.6e9
 Z_stub_2 = 1j * Z_c * tan(2 * pi * (lambda_actual/4)/(CONST_C / f_off / sqrt(epsilon_eff)))
 print('Z_stub({:.3E} Hz) = {:.3E}'.format(f_off, Z_stub_2))
 
+beta = 2 * pi / lambda_actual
+
 # working right to left
-Z_1 = Z_stub_2
+Z_1 = parallel(Z_stub_2, Z_c)
+Gamma_1L = (Z_1 - Z_c) / (Z_1 + Z_c)
 Z_2 = Z_c + Z_1
 Z_3 = parallel(Z_stub_2, Z_2)
 Z_4 = Z_c + Z_3
 Z_5 = parallel(Z_stub_2, Z_4)
 Z_in = Z_5
 print('Z_in = {:.3f}'.format(Z_in))
-
-# working right to left
-#Gamma_1 = reflection_coeff(parallel(50, Z_stub_2), 50)
-#print('Gamma_1 = {:.3f}'.format(Gamma_1))
-#Z_1 = Z_c * (1 + Gamma_1 * e**(-1j * 4 * pi * (lambda_actual/4)/(CONST_C / 2.6e9 / sqrt(epsilon_eff)))/(1 - Gamma_1 * e**(-1j * 4 * pi * (lambda_actual/4)/(CONST_C / 2.6e9 / #sqrt(epsilon_eff)))))
-#print('Z_1 = {:.3f}'.format(Z_1))
-#Gamma_2 = reflection_coeff(parallel(Z_1, Z_stub_2), 50)
-#Z_2 = Z_c * (1 + Gamma_2 * e**(-1j * 4 * pi * (lambda_actual/4)/(CONST_C / 2.6e9 / sqrt(epsilon_eff)))/(1 - Gamma_2 * e**(-1j * 4 * pi * (lambda_actual/4)/(CONST_C / 2.6e9 / #sqrt(epsilon_eff)))))
-##print('Z_2 = {:.3f}'.format(Z_2))
-#Z_in = parallel(Z_2, Z_stub_2)
-#print('Z_in = {:.3f}'.format(Z_in))
-#Z_seen = 50 + 50**2 / Z_in
 
 # s parameters for parallel impedance
 s_21 = 2 * Z_c / (2 * Z_in + Z_c)
@@ -104,6 +96,116 @@ NF_fefilter = 0.1 # dB
 ##########################################################################################################################
 print('\n================ First Amp ================')
 
+V_CC = 13.0 # V
+I_C = 10.0e-3 # A
+V_CE = 2.0 # V
+V_BE = 0.7 # V
+beta_min = 160.0
+beta_max = 400.0
+beta = 280.0 # 160 - 400
+
+R_C = (V_CC - V_BE) / I_C
+print('R_C = {:.3E}'.format(R_C))
+
+R_C = 1100 # Ohm
+
+R_1_smallest = beta_min * R_C
+print('R_1_smallest = {:.3E}'.format(R_1_smallest))
+
+R_1 = 20.5e3
+I_C = (V_CC - V_BE) / (R_C + (R_1 + R_C)/beta_min)
+I_B = ((V_CC - V_BE) - R_C * I_C) / (R_C + R_1)
+V_CE = V_CC - (I_C + I_B) * R_C
+P = V_CE * I_C
+
+print('Final values:')
+print('R_C = {:.3E}'.format(R_C))
+print('R_1 = {:.3f}'.format(R_1))
+print('I_B = {:.3E}'.format(I_B))
+print('I_C = {:.3E}'.format(I_C))
+print('V_CE = {:.3f} V'.format(V_CE))
+print('Power = {:.3E} W'.format(P))
+
+print('Blocking Cap:')
+C_block = 100.0e-12
+Z_block = -1j / (2 * pi * f_center * C_block)
+print('C_block = {:.3E}'.format(C_block))
+print('Z_block = {}'.format(Z_block))
+
+# assume approximately unilateral (for now)
+s_12 = 0.01
+
+# assume V_A (early voltage value)
+V_A = 100
+
+s_21_2_dB = 20 # dB
+s_21_2 = 10**(s_21_2_dB / 10)
+s_21 = sqrt(s_21_2)
+print('s_21^2 = {:.3f}'.format(s_21_2))
+print('s_21 = {:.3f}'.format(s_21))
+
+print('Calculating s_11:')
+C_BE = 400.0e-15
+r_i = beta / (40 * 0.04)
+print('r_i = {:.3E}'.format(r_i))
+Z_BE = -1j / (2 * pi * f_center * C_BE) + r_i
+print('Z_BE = {} = {}'.format(Z_BE, cmath.polar(Z_BE)))
+print('ang = {:.6f}'.format(degrees(cmath.polar(Z_BE)[1])))
+
+print('Calculating s_22:')
+r_o = (V_A + V_CE) / I_C
+print('r_o = {:.3E}'.format(r_o))
+C_CE = 268.0e-15
+Z_CE = parallel(-1j / (2 * pi * f_center * C_CE), r_o)
+print('Z_CE = {} = {}'.format(Z_CE, cmath.polar(Z_CE)))
+print('ang = {:.6f}'.format(degrees(cmath.polar(Z_CE)[1])))
+
+print('Calculating s_21:')
+Z_CB = parallel(Z_BE, Z_CE) * s_21
+print('Z_CB = {} = {}'.format(Z_CB, cmath.polar(Z_CB)))
+print('ang = {:.6f}'.format(degrees(cmath.polar(Z_CB)[1])))
+
+print('Normalize values to s_21:')
+factor = (Z_CB / cmath.polar(Z_CB)[0])
+s_11 = Z_BE / cmath.polar(Z_CB)[0]
+s_22 = Z_CE / cmath.polar(Z_CB)[0]
+s_21 *= factor
+print('s_21 = {:.3f} = {:.3f} /_ {:.3f}'.format(s_21, abs(s_21), degrees(cmath.polar(s_21)[1])))
+print('s_11 = {:.3f} = {:.3f} /_ {:.3f}'.format(s_11, abs(s_11), degrees(cmath.polar(s_11)[1])))
+print('s_22 = {:.3f} = {:.3f} /_ {:.3f}'.format(s_22, abs(s_22), degrees(cmath.polar(s_22)[1])))
+
+print('Stability')
+Gamma_S = s_11.conjugate()
+Gamma_L = s_22.conjugate()
+Gamma_in = s_11 + (s_12 * s_21 * Gamma_L) / (1 - s_22 * Gamma_L)
+Gamma_out = s_22 + (s_12 * s_21 * Gamma_S) / (1 - s_11 * Gamma_S)
+delta = s_11 * s_22 - s_12 * s_21
+K = (1 + abs(delta)**2 + abs(s_11)**2 + abs(s_22)**2) / (2 * abs(s_21 * s_12))
+print('|delta| = {:.3f}'.format(abs(delta)))
+print('K = {:.3f}'.format(K))
+print('|Gamma_S| = {}'.format(abs(Gamma_S)))
+print('|Gamma_L| = {}'.format(abs(Gamma_L)))
+print('|Gamma_in| = {}'.format(abs(Gamma_in)))
+print('|Gamma_out| = {}'.format(abs(Gamma_out)))
+
+print('Gain:')
+G_0 = abs(s_21)**2
+G_1 = (1 - abs(Gamma_S)**2) / abs(1 - s_11 * Gamma_S)**2
+G_2 = (1 - abs(Gamma_L)**2) / abs(1 - s_22 * Gamma_L)**2
+print(G_0)
+print(G_1)
+print(G_2)
+G_TU = G_1 * G_0 * G_2
+print('G_TU = {} = {:.3f} dB'.format(G_TU, 10 * log10(G_TU)))
+
+NF_amp1 = (0.7 + 0.47) / 2
+
+##########################################################################################################################
+# second amp
+##########################################################################################################################
+print('\n================ Second Amp ================')
+
+NF_amp2 = (0.7 + 0.47) / 2
 
 ##########################################################################################################################
 # post-mixer filter
@@ -169,5 +271,12 @@ attenuation = 11.749 / 100.0 # per m @ 400 MHz
 print('\n================ System ================')
 print('Noise Factors:')
 print('Front End Filter = {:.3f}'.format(NF_fefilter))
+print('Amp 1 = {:.3f}'.format(NF_amp1))
+print('Amp 2 = {:.3f}'.format(NF_amp2))
 print('Post Mix Filter = {:.3f}'.format(NF_pmfilter))
 print('Bias tee = {:.3f}'.format(NF_biastee))
+
+# TODO - this calculation is wrong
+NF_total = NF_fefilter + NF_amp1 + NF_amp2 + NF_pmfilter + NF_biastee
+
+print('Total NF = {:.3f}'.format(NF_total))
